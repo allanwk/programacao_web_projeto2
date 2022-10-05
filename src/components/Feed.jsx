2a2294738b2552dca8bc90ca4bbb9caf9b5e1b40
Allan <allan.kretzmann@exati.com.br>
2022-10-05 19:15:44 -0300

    Layout basico

---
+++ src/components/Feed.jsx
@@ -0,0 +1,183 @@
+export default function Feed() {
+  return (
+    <section id="stories">
+      <div class="main-stories">
+        <div class="main-left">
+          <a href="#">
+            <img src="assets/fishes.webp" />
+            <div class="text-container">
+              <h1>Gone smishing: Scams move to the workplace</h1>
+              <h2>Ignore the gift card request from your boss (and ours).</h2>
+              <p class="author">Sherry Qin</p>
+            </div>
+          </a>
+        </div>
+
+        <div class="main-right">
+          <a href="#" class="wrapper-link">
+            <div class="text">
+              <h1 class="headline">
+                Meet the guy uncovering crypto’s biggest thefts
+              </h1>
+              <h2 class="subtitle">
+                Rich Sanders is the closest thing the cryptoworld has to 911.
+              </h2>
+              <p class="author">Amanda Hoover</p>
+            </div>
+            <div class="img-container">
+              <img src="assets/crypto.webp" />
+            </div>
+          </a>
+          <a href="#" class="wrapper-link">
+            <div class="text">
+              <h1 class="headline">Why rich people are shopping at Walmart</h1>
+              <h2 class="subtitle">They’re also eating at Applebee’s.</h2>
+              <p class="author">ABIGAIL RUBENSTEIN</p>
+            </div>
+            <div class="img-container">
+              <img src="assets/knees.webp" />
+            </div>
+          </a>
+          <a href="#" class="wrapper-link">
+            <div class="text">
+              <h1 class="headline">
+                Haute couture is moving from Paris to the metaverse
+              </h1>
+              <h2 class="subtitle">Fashion's next frontier is digital.</h2>
+              <p class="author">SHERRY QIN</p>
+            </div>
+            <div class="img-container">
+              <img src="assets/haute.gif" />
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="latest-stories">
+        <h2>Latest Stories</h2>
+        <div class="latest-stories-grid">
+          <a href="#" class="card wrapper-link">
+            <img src="assets/crypto.webp" />
+            <p class="tag">International</p>
+            <h1 class="headline">
+              Meet the guy uncovering crypto’s biggest thefts
+            </h1>
+            <p class="author light">Amanda Hoover</p>
+          </a>
+
+          <a href="#" class="card wrapper-link">
+            <img src="assets/knees.webp" />
+            <p class="tag">International</p>
+            <h1 class="headline">Why rich people are shopping at Walmart</h1>
+            <p class="author light">ABIGAIL RUBENSTEIN</p>
+          </a>
+
+          <a href="#" class="card wrapper-link">
+            <img src="assets/haute.gif" />
+            <p class="tag">International</p>
+            <h1 class="headline">
+              Haute couture is moving from Paris to the metaverse
+            </h1>
+            <p class="author light">SHERRY QIN</p>
+          </a>
+
+          <a href="#" class="card wrapper-link">
+            <img src="assets/crypto.webp" />
+            <p class="tag">International</p>
+            <h1 class="headline">
+              Meet the guy uncovering crypto’s biggest thefts
+            </h1>
+            <p class="author light">Amanda Hoover</p>
+          </a>
+
+          <a href="#" class="card wrapper-link">
+            <img src="assets/knees.webp" />
+            <p class="tag">International</p>
+            <h1 class="headline">Why rich people are shopping at Walmart</h1>
+            <p class="author light">ABIGAIL RUBENSTEIN</p>
+          </a>
+
+          <a href="#" class="card wrapper-link">
+            <img src="assets/haute.gif" />
+            <p class="tag">International</p>
+            <h1 class="headline">
+              Haute couture is moving from Paris to the metaverse
+            </h1>
+            <p class="author light">SHERRY QIN</p>
+          </a>
+        </div>
+      </div>
+
+      <div class="more-stories">
+        <h2>More Stories</h2>
+        <div class="more-stories-grid">
+          <a class="sideways-card wrapper-link" href="#">
+            <img src="assets/crypto.webp" />
+            <div class="side-text">
+              <p class="tag">International</p>
+              <h1 class="headline">
+                Meet the guy uncovering crypto’s biggest thefts
+              </h1>
+              <p class="author light">Amanda Hoover</p>
+            </div>
+          </a>
+
+          <a class="sideways-card wrapper-link" href="#">
+            <img src="assets/knees.webp" />
+            <div class="side-text">
+              <p class="tag">International</p>
+              <h1 class="headline">
+                Meet the guy uncovering crypto’s biggest thefts
+              </h1>
+              <p class="author light">Amanda Hoover</p>
+            </div>
+          </a>
+
+          <a class="sideways-card wrapper-link" href="#">
+            <img src="assets/haute.gif" />
+            <div class="side-text">
+              <p class="tag">International</p>
+              <h1 class="headline">
+                Haute couture is moving from Paris to the metaverse
+              </h1>
+              <p class="author light">SHERRY QIN</p>
+            </div>
+          </a>
+
+          <a class="sideways-card wrapper-link" href="#">
+            <img src="assets/crypto.webp" />
+            <div class="side-text">
+              <p class="tag">International</p>
+              <h1 class="headline">
+                Meet the guy uncovering crypto’s biggest thefts
+              </h1>
+              <p class="author light">Amanda Hoover</p>
+            </div>
+          </a>
+
+          <a class="sideways-card wrapper-link" href="#">
+            <img src="assets/knees.webp" />
+            <div class="side-text">
+              <p class="tag">International</p>
+              <h1 class="headline">
+                Meet the guy uncovering crypto’s biggest thefts
+              </h1>
+              <p class="author light">Amanda Hoover</p>
+            </div>
+          </a>
+
+          <a class="sideways-card wrapper-link" href="#">
+            <img src="assets/haute.gif" />
+            <div class="side-text">
+              <p class="tag">International</p>
+              <h1 class="headline">
+                Haute couture is moving from Paris to the metaverse
+              </h1>
+              <p class="author light">SHERRY QIN</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
